7cb743bea255fc6acfce9025e9599836ab6c2745
JhonReimberg <joaoreimberg09@gmail.com>
2021-09-23 09:51:25 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoFinal/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace ProjetoFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NomeDaMaquina = new System.Windows.Forms.Label();
+            this.txtNomeM = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtST = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtNum = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NomeDaMaquina
+            // 
+            this.NomeDaMaquina.Location = new System.Drawing.Point(10, 138);
+            this.NomeDaMaquina.Name = "NomeDaMaquina";
+            this.NomeDaMaquina.Size = new System.Drawing.Size(174, 25);
+            this.NomeDaMaquina.TabIndex = 0;
+            this.NomeDaMaquina.Text = "NOME DA MAQUINA:\r\n";
+            this.NomeDaMaquina.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNomeM
+            // 
+            this.txtNomeM.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeM.Location = new System.Drawing.Point(148, 138);
+            this.txtNomeM.Name = "txtNomeM";
+            this.txtNomeM.Size = new System.Drawing.Size(276, 22);
+            this.txtNomeM.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(148, 180);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(276, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // txtST
+            // 
+            this.txtST.Location = new System.Drawing.Point(11, 180);
+            this.txtST.Name = "txtST";
+            this.txtST.Size = new System.Drawing.Size(174, 25);
+            this.txtST.TabIndex = 2;
+            this.txtST.Text = "SERVICE TAG:\r\n";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(148, 229);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(276, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(11, 229);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(174, 25);
+            this.txtNum.TabIndex = 4;
+            this.txtNum.Text = "Nº CONTROLE:";
+            this.txtNum.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(148, 273);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(11, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "USUÁRIO ATUAL:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetoFinal.Properties.Resources.download;
+            this.pictureBox1.Location = new System.Drawing.Point(-61, -15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(834, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(223, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(489, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 71);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Visualizar Cadastros";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 370);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtST);
+            this.Controls.Add(this.txtNomeM);
+            this.Controls.Add(this.NomeDaMaquina);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "-";
+            this.TransparencyKey = System.Drawing.Color.RoyalBlue;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NomeDaMaquina;
+        private System.Windows.Forms.TextBox txtNomeM;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label txtST;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label txtNum;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
